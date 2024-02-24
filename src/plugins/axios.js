f34c80a1225a2927efdb2813df2878e745645e58
Zahir Bachnou <zahir.bachnou@protonmail.com>
2024-02-24 11:08:46 +0100

    Change backend url

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://zahirdcg.fr', // Remplace avec l'URL de ton backend
+  baseURL: 'http://api.zahirdcg.fr', // Remplace avec l'URL de ton backend
 });
 
 export default instance;
